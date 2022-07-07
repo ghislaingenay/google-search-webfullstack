a9f25d214f735fef699a7052969316eb6742e977
Ghislain Genay <ghislaingenay@gmail.com>
2022-07-07 14:13:42 +0700

    Add a search Schema

--- models/results.js
+++ models/results.js
@@ -1,3 +1,25 @@
 const mongoose = require('mongoose')
 
 // Create the results moodel
+
+const searchSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    require: true
+  },
+  description: {
+    type: String,
+    require: true,
+  },
+  url: {
+    type: String,
+    require: true,
+  },
+  links: [{
+    title: String,
+    url: String,
+  }]
+}
+
+const Search = mongoose.model("Search", searchSchema)
+
