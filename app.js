612cdb131da40f5675b66ee154efa9b52498eb04
Ghislain Genay <ghislaingenay@gmail.com>
2022-07-07 14:13:08 +0700

    Add connection to mongoose

--- app.js
+++ app.js
@@ -38,40 +38,42 @@ app.use(methodOverride('_method'))
 app.use(express.static(path.join(__dirname, 'public')))
 
 // Database
-// mongoose.connect(
-//   process.env.MONGODB_URL,
-//   { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false },
-//   () => {
-//     console.log('Connected to MongoDB')
-//   }
-// )
+mongoose.connect(
+  process.env.MONGODB_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false
+  },
+  () => {
+    console.log('Connected to MongoDB')
+  }
+)
 
-// ...
+    // ...
 
-// Routes
-const search = require('./controllers/search.js');
-app.use('/', search);
-
-
-const results = require('./controllers/results.js')
-app.use('/results', results);
+    // Routes
+    const search = require('./controllers/search.js'); app.use('/', search);
 
 
-// Catch 404 and forward to error handler
-app.use((req, res, next) => {
-  next(createError(404))
-})
+    const results = require('./controllers/results.js')
+    app.use('/results', results);
 
-// Error Handler
-app.use((err, req, res, next) => {
-  // Only provides full error in development
-  res.locals.message = err.message
-  res.locals.error = req.app.get('env') === 'development' ? err : {}
-  res.locals.coder = req.coder
-  res.locals.hideSearch = true
-  // Render the error page
-  res.status(err.status || 500)
-  res.render('error')
-})
 
-module.exports = app
+    // Catch 404 and forward to error handler
+    app.use((req, res, next) => {
+      next(createError(404))
+    })
+
+    // Error Handler
+    app.use((err, req, res, next) => {
+      // Only provides full error in development
+      res.locals.message = err.message
+      res.locals.error = req.app.get('env') === 'development' ? err : {}
+      res.locals.coder = req.coder
+      res.locals.hideSearch = true
+      // Render the error page
+      res.status(err.status || 500)
+      res.render('error')
+    })
+
+    module.exports = app
