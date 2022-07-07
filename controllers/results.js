ed80a20ba6498095782c884a89284b99181c82f1
Ghislain Genay <ghislaingenay@gmail.com>
2022-07-07 14:44:14 +0700

    Add the find function filter - Test 1

--- controllers/results.js
+++ controllers/results.js
@@ -3,52 +3,30 @@ const express = require('express')
 const router = express.Router()
 
 const filtering = require("../services/filtering.js")
+const Results = require("../models/results.js")
 
-let data = [{
-        title: 'JS tutorials Hello',
-        description: 'The best JavaScript tutorials in the galaxy!',
-        url: 'https://www.w3schools.com',
-        links: [{
-            title: 'JS for Beginners',
-            url: 'https://www.w3schools.com/js'
-        }, {
-            title: 'JS for the Web',
-            url: 'https://www.w3schools.com/js'
-        }]
-    }, {
-        title: 'JavaScript | MDN',
-        description: "JavaScript is a lightweight, interpreted, or just-in-time compiled programming language with first - class ",
-        url: 'developer.mozilla.org',
-        links: [{
-            title: 'JavaScript Operator',
-            url: 'https://www.w3schools.com/js'
-        }, {
-            title: 'JS for the Web',
-            url: 'https://www.w3schools.com/js'
-        }]
-    },
-    {
-        title: 'JavaScript - Wikipedia - Tortuga',
-        description: "JavaScript is often abbreviated as JS, is a programming language that conforms to the ECMAScript specification interpreted, or just-in-time compiled programming language with first - class ",
-        url: 'en.wikipedia.org',
-        links: [{
-            title: 'JavaScript Operator',
-            url: 'https://www.w3schools.com/js'
-        }, {
-            title: 'JS for the Web',
-            url: 'https://www.w3schools.com/js'
-        }]
-    },
-]
+
+const searchResults = async () => {
+    return Results.find({
+        title: req.body.search,
+        description: req.body.search,
+        url: req.body.search,
+    })
+}
 
 router.get("/", (req, res) => {
     res.render("results")
 })
 
 router.post("/", (req, res) => {
-    res.render("results", {
-        results: filtering(data, req.body.search)
-    })
+    res.render("results", searchResults)
 })
+
+
+// router.post("/", (req, res) => {
+//     res.render("results", {
+//         results: filtering(data, req.body.search)
+//     })
+// })
 
 module.exports = router
