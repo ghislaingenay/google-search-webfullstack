9f753f9ffa0d126c6c99aac23727e063ea1e3d36
Ghislain Genay <ghislaingenay@gmail.com>
2022-07-08 09:06:42 +0700

    Add regex to search elements in title

--- controllers/results.js
+++ controllers/results.js
@@ -10,11 +10,58 @@ router.get("/", (req, res) => {
     res.render("results")
 })
 
+// router.post("/", async (req, res) => {
+//     let resultArray =  await Results.find({
+//         title: req.body.search})
+//     res.render("results", {results: resultArray})
+// })
+
 router.post("/", async (req, res) => {
-    let resultArray =  await Results.find({
-        title: req.body.search})
+    let reg = new RegExp(req.body.search)
+    let resultArray = await Results.find({
+                'title': {
+                    $regex: reg,
+                    $options: "i"
+                }
+            })
+    console.log(resultArray)
     res.render("results", {results: resultArray})
 })
+
+
+
+// router.post("/", async (req, res) => {
+//     let reg = new RegExp(req.body.search)
+//     let resultArray = Results.find({
+//         $or: [{
+//                 title: {
+//                     $regex: reg,
+//                     $options: "gi"
+//                 }
+//             },
+//             {
+//                 description: {
+//                     $regex: reg,
+//                     $options: "gi"
+//                 }
+//             },
+//             {
+//                 url: {
+//                     $regex: reg,
+//                     $options: "gi"
+//                 }
+//             },
+//             err => {
+//                 console.log(err)
+//             }
+//         ]
+//     })
+//     console.log(resultArray)
+//     res.render("results", {
+//         results: resultArray
+//     })
+// })
+
 
 
 module.exports = router
