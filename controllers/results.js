2a08c3d59a388396aa529214ec8f04d4f77b66cd
Ghislain Genay <ghislaingenay@gmail.com>
2022-07-08 14:57:37 +0700

    Add query using search index

--- controllers/results.js
+++ controllers/results.js
@@ -10,31 +10,47 @@ router.get("/", (req, res) => {
     res.render("results")
 })
 
+// router.post("/", async (req, res, next) => {
+//     try {
+//         let reg = new RegExp(req.body.search)
+//         let resultArray = await Results.find({
+//             $or: [{
+//                     title: {
+//                         $regex: reg,
+//                         $options: "gi"
+//                     }
+//                 },
+//                 {
+//                     description: {
+//                         $regex: reg,
+//                         $options: "gi"
+//                     }
+//                 },
+//                 {
+//                     url: {
+//                         $regex: reg,
+//                         $options: "gi"
+//                     }
+//                 }
+//             ]
+
+//         }, )
+//         res.render("results", {
+//             results: resultArray
+//         })
+//     } catch (err) {
+//         next(err)
+//     }
+// })
+
 router.post("/", async (req, res, next) => {
     try {
-        let reg = new RegExp(req.body.search)
         let resultArray = await Results.find({
-            $or: [{
-                    title: {
-                        $regex: reg,
-                        $options: "gi"
-                    }
-                },
-                {
-                    description: {
-                        $regex: reg,
-                        $options: "gi"
-                    }
-                },
-                {
-                    url: {
-                        $regex: reg,
-                        $options: "gi"
-                    }
-                }
-            ]
-
-        }, )
+            $text: {
+                $search: req.body.search,
+            }
+        })
+        console.log(resultArray)
         res.render("results", {
             results: resultArray
         })
@@ -42,19 +58,6 @@ router.post("/", async (req, res, next) => {
         next(err)
     }
 })
-
-// router.post("/", async (req, res, next) => {
-//     try {
-//         let resultArray = await Results.aggregate([{
-//             $search: { "text":{
-//                 "query": req.body.search,
-//                 "path": "description"
-//             }}
-//         }, ])
-//     } catch (err) {
-//         next(err)
-//     }
-// })
 
 
 module.exports = router
